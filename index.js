261293701b7cd9eefb3ceedcc25a38dfd56d181f
subham007-coder <dsubham109@gmail.com>
2024-04-26 15:21:25 +0530

    index file

--- index.js
+++ index.js
@@ -1,51 +1,130 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const userModel = require('./models/user.models');
+// const PORT = process.env.PORT || 3000;
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.set('view engine', "ejs");
+
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
+
+// app.get('/show', async (req, res) => {
+//   let allUser = await userModel.find();
+//   res.render('show', {allUser});
+// });
+
+// app.post('/create', async (req, res) => {
+//   let {name, email, image} = req.body;
+//   let makeUser = await userModel.create({
+//     name: name,
+//     email: email,
+//     image: image,
+//   });
+//   res.redirect('/show');
+// });
+
+// app.get('/edit/:id', async (req, res) => {
+//   let allUser = await userModel.findOne({_id: req.params.id});
+//   res.render('edit', {allUser});
+// });
+
+// app.post('/edit/:id', async (req, res, next) => {
+//   const { name, email, image } = req.body;
+//   let user = await userModel.findOneAndUpdate({_id: req.params.id}, {name, email, image}, {new: true});
+//   res.redirect('/show');
+// });
+
+// app.get('/delete/:id', async (req, res) => {
+//   let user = await userModel.findOneAndDelete({_id: req.params.id});
+//   console.log(user);
+//   res.redirect('/show');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`listening on port http://localhost:${PORT}/`);
+// });
+
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const userModel = require('./models/user.models');
 const PORT = process.env.PORT || 3000;
 
-
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', "ejs");
+app.set('view engine', 'ejs');
 
+// Routes
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/show', async (req, res) => {
-  let allUser = await userModel.find();
-  res.render('show', {allUser});
+app.get('/show', async (req, res, next) => {
+  try {
+    const allUser = await userModel.find();
+    res.render('show', { allUser });
+  } catch (error) {
+    next(error); // Pass error to error handling middleware
+  }
 });
 
-app.post('/create', async (req, res) => {
-  let {name, email, image} = req.body;
-  let makeUser = await userModel.create({
-    name: name,
-    email: email,
-    image: image,
-  });
-  res.redirect('/show');
+// Add input validation middleware for '/create' and '/edit/:id' routes
+
+app.post('/create', async (req, res, next) => {
+  try {
+    const { name, email, image } = req.body;
+    const makeUser = await userModel.create({ name, email, image });
+    res.redirect('/show');
+  } catch (error) {
+    next(error); // Pass error to error handling middleware
+  }
 });
 
-app.get('/edit/:id', async (req, res) => {
-  let allUser = await userModel.findOne({_id: req.params.id});
-  res.render('edit', {allUser});
+app.get('/edit/:id', async (req, res, next) => {
+  try {
+    const allUser = await userModel.findOne({ _id: req.params.id });
+    res.render('edit', { allUser });
+  } catch (error) {
+    next(error); // Pass error to error handling middleware
+  }
 });
 
 app.post('/edit/:id', async (req, res, next) => {
-  const { name, email, image } = req.body;
-  let user = await userModel.findOneAndUpdate({_id: req.params.id}, {name, email, image}, {new: true});
-  res.redirect('/show');
+  try {
+    const { name, email, image } = req.body;
+    const user = await userModel.findOneAndUpdate({ _id: req.params.id }, { name, email, image }, { new: true });
+    res.redirect('/show');
+  } catch (error) {
+    next(error); // Pass error to error handling middleware
+  }
 });
 
-app.get('/delete/:id', async (req, res) => {
-  let user = await userModel.findOneAndDelete({_id: req.params.id});
-  console.log(user);
-  res.redirect('/show');
+app.get('/delete/:id', async (req, res, next) => {
+  try {
+    const user = await userModel.findOneAndDelete({ _id: req.params.id });
+    res.redirect('/show');
+  } catch (error) {
+    next(error); // Pass error to error handling middleware
+  }
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Server Setup
 app.listen(PORT, () => {
-  console.log(`listening on port http://localhost:${PORT}/`);
+  console.log(`Listening on port http://localhost:${PORT}/`);
 });
+
