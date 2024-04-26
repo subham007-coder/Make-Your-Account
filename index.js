faed7f136e18daa15a761e8e0f93a692665e5b0a
subham007-coder <dsubham109@gmail.com>
2024-04-26 15:02:15 +0530

    some configurations

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const userModel = require('./models/user.models');
-const PORT = 3500;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
